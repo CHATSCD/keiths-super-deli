7f035ccf2a20a48bba77da29e421c8b879d3899f
CHATSCD <shaundubuisson24@gmail.com>
2026-02-07 20:27:20 -0600

    Smarter Production App - Initial commit

--- next.config.mjs
+++ next.config.mjs
@@ -1,12 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
   reactStrictMode: true,
-  swcMinify: true,
-  output: 'standalone',
-  staticPageGenerationTimeout: 180,
-  webpack: (config) => {
-    return config;
-  },
 };
 
 export default nextConfig;
